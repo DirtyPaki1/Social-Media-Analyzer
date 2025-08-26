ecd91805fb7c17d8c9a686634a837cfe2479da01
Usman Qidwai <usmanqidwai21@gmail.com>
2025-08-25 22:55:27 -0400

    Work

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          background: "rgb(var(--background))",
-          foreground: "rgb(var(--foreground))",
-          primary: "rgb(var(--primary))",
-          border: "rgb(var(--border))",
-          muted: "rgb(var(--muted))",
-          "muted-foreground": "rgb(var(--muted-foreground))",
-        },
-      },
-    },
-    plugins: [],
-  }
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
